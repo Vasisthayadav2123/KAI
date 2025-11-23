936511b091dedc55715ec21be8f89cd405b13321
Vasisthayadav2123 <vasisthayadav58@gmail.com>
2025-11-23 23:40:55 +0530

    audio track foundation

--- backend/serverStream.py
+++ backend/serverStream.py
@@ -13,10 +13,72 @@ import subprocess
 """""""""
 ------> Gpu screen track for low latency
 """""""""
-class GPUScreenTrack(VideoStreamTrack);
+class GPUScreenTrack(VideoStreamTrack):
     def __init__(self, monitor=None, target_fps=60):
         super().__init__()
         self.sct = mss.mss()
+        self.monitor = monitor or self.sct.monitors[1]
+        self.frame_interval = 1/ target_fps
+        self.last_time = 0
+        self.running = True
+
+        img = self.sct.grab(self.monitor)
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
+        self.first_frame = frame
+
+    async def recv(self):
+        if not self.running:
+            await asyncio.sleep(0.05)
+            return await self.recv()
+        
+        now = time.time()
+        elapsed = now - self.last_time
+        if elapsed < self.frame_interval:
+            await asyncio.sleep(self.frame_interval - elapsed)
+        self.last_time = time.time()
+
+        #get screen frame
+        img = self.sct.grab(self.monitor)
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
+
+        # attacht to webrtc frame
+        video_frame = VideoFrame.from_ndarray(frame, format="rgb24")
+        video_frame.pts, video_frame.time_base = await self.next_timestamp()
+        return video_frame
+    
+    def stop(self):
+        self.running = False
+        super().stop()
+
+
+
+
+"""""""""
+------> Audio track from system audio
+"""""""""
+
+
+
+class systemAudioTrack(AudioStreamTrack):
+    kind = "audio"
+    def __init__(self):
+        super().__init__()
+
+        ffmpeg_cmd = [
+            "ffmpeg",
+            "-f", "dshow",
+            "-i",
+            "audio=Stereo Mix (Realtek(R) Audio)", 
+            "-ac",
+            "2",
+            "-ar",
+            "48000",
+            "-f",
+            "s16le",
+            "pipe:1"
+        ]
 
 
 
